3618a2b29d708a92546b0dba7235851f3999408c
Christoph Kepler <development@kepler.international>
2021-05-05 22:39:16 +0200

    Rewrite js theme switcher to accommodate os-themes
    The switcher now follows reports from the browser/os but overrides it as
    soon as interaction with the switcher is manually made.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,40 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme');
+const matchMediaPrefLight = window.matchMedia('(prefers-color-scheme: light)');
+var currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
 	document.documentElement.setAttribute('data-theme', currentTheme);
 
-	if (currentTheme === 'light') {
+	if (currentTheme === 'light' || matchMediaPrefLight.matches) {
 		toggleSwitch.checked = true;
 	}
 }
 
 function switchTheme(e) {
 	if (e.target.checked) {
-		document.documentElement.setAttribute('data-theme', 'light');
-		localStorage.setItem('theme', 'light');
+		currentTheme = 'light';
 	}
 	else {
-		document.documentElement.setAttribute('data-theme', 'dark');
-		localStorage.setItem('theme', 'dark');
+		currentTheme = 'dark';
+	}
+	document.documentElement.setAttribute('data-theme', currentTheme);
+	localStorage.setItem('theme', currentTheme);
+}
+
+function onSystemThemeChange(e) {
+	if (currentTheme) {
+		return;
+	}
+	if (e.matches) {
+		toggleSwitch.checked = true;
+	}
+	else {
+		toggleSwitch.checked = false;
 	}
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+matchMediaPrefLight.addEventListener('change', onSystemThemeChange)
 
 var easteregg = function(){
   console.log(`%c Welcome to`, "color:#00050f; font-family:monospace");
